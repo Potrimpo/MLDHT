1a3b749c0546e2027d82e133b65b7d11e840c3f3
Lewis Knox Streader <lewis.knoxstreader@gmail.com>
2017-07-18 12:00:18 +1200

    added docs for Namespace.name

--- lib/mldht/namespace.ex
+++ lib/mldht/namespace.ex
@@ -2,8 +2,10 @@ defmodule MlDHT.Namespace do
 
   @name __MODULE__
 
-  def name(module, id), do: { :via, Registry, { @name, { module, id } } }
-
-  def lookup(module, id), do: Registry.lookup(@name, {module, id})
+  @doc """
+  Creates a via tuple for naming new and accessing existing processes.
+  Takes the desired name (usually @name) and the node_id of the DHT node we're running.
+  """
+  def name(name, id), do: { :via, Registry, { @name, { name, id } } }
 
 end
